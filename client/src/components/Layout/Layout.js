30b407452bdb5ea92593c3ed1cd5c6095a7a5737
selimnill <mrrafi812345@gmail.com>
2024-01-26 20:35:00 +0600

    reinitialized

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Helmet } from "react-helmet";
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Header />
       <main style={{ minHeight: "70vh" }}>{children}</main>
       <Footer />
